2ce66f725871ee8b5b948076ef04a66e3ffff631
briPod <33463220+br1Pod@users.noreply.github.com>
2021-11-02 09:09:48 +0000

    seeds now working"

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,20 +1,36 @@
 use crm;
 db.dropDatabase();
 
-db.clients.insertMany([
+db.clients.insertMany([    
   {
     first_name: "John",
     last_name: "Lennon",
-    phone: 0123 456 789,
+    phone: "0123 456 789",
     email: "john@beatles.com",
     address: "1 Penny Lane, Liverpool, L1 1AA",
     emergency_name: "Julia Lennon",
-    emergency_phone: 0123 456 789;
+    emergency_phone: "0123 456 789",
     emergency_aware: "yes",
     wew_score: 0,
     total_sessions: 12,
     completed_sessions: 3,
-    finished: no,
+    finished: "no",
+    hourly_rate: "full_rate",
+    client_type: "private"
+  },
+  {
+    first_name: "Paul",
+    last_name: "McCartney",
+    phone: "0123 456 111",
+    email: "paul@beatles.com",
+    address: "2 Penny Lane, Liverpool, L1 2BB",
+    emergency_name: "Linda McCartney",
+    emergency_phone: "0123 456 111",
+    emergency_aware: "no",
+    wew_score: 2,
+    total_sessions: 9,
+    completed_sessions: 4,
+    finished: "no",
     hourly_rate: "full_rate",
     client_type: "private"
   }
