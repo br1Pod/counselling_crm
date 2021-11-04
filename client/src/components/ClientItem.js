53f95dc484ab29bde0c41292f695b069f0c915ea
briPod <33463220+br1Pod@users.noreply.github.com>
2021-11-04 09:39:55 +0000

    css added, expanding panel

--- client/src/components/ClientItem.js
+++ client/src/components/ClientItem.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { deleteClient, updateClient } from "./forms/AddClientService"
 import EditClientForm from '../components/forms/EditClientForm'
+import Collapsible from 'react-collapsible';
 
 const ClientItem = ({client, removeClient}) => {
     const [clients, setClients] = useState([]);
@@ -20,14 +21,43 @@ const ClientItem = ({client, removeClient}) => {
     return (
         <>
         <li>
+            <div>
             <a href="" className="person-item, list-object">{client.first_name} {client.last_name}</a>
             <span className="person-manage">
-            <button className="list-object" onClick={handleChange}>view</button>
             <button className="list-object" onClick={handleDelete}>x</button>
             </span>
-            {/* <ul className="editable-form">
-                <li><EditClientForm updateClient={updateClient} client={client} /></li>
-            </ul> */}
+            </div>
+
+            <Collapsible trigger="expand" className="list-object">
+                <div className="content"> 
+                <form>
+                <label>Case Note</label>
+                <textarea>Add Case notes in here</textarea>
+                </form>
+
+                <p>Name: {client.first_name} {client.last_name} <span id="edit">edit</span></p>
+                <p>Phone: {client.phone} <span id="edit">edit</span></p>
+                <p>Email: {client.email} <span id="edit">edit</span></p>
+                <p>Address: {client.address} <span id="edit">edit</span></p>
+                <p>Emergency Contact: {client.emergency_name} <span id="edit">edit</span></p>
+                <p>Emergency Tel: {client.emergency_phone} <span id="edit">edit</span></p>
+
+                <p>Name: {client.wew_score} <span id="edit">edit</span></p>
+                <p>Name: {client.total_sessions} <span id="edit">edit</span></p>
+                <p>Name: {client.completed_sessions} <span id="edit">edit</span></p>
+                <p>Name: {client.hourly_rate} <span id="edit">edit</span></p>
+                <p>Name: {client.client_type} <span id="edit">edit</span></p>
+                
+                <p> <span id="edit">edit</span></p>
+                </div>
+            </Collapsible>
+            
+            
+
+            
+
+
+            
         </li>       
         <hr></hr>
         </>
